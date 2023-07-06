c5f701f771fb06f629d635d32e20cf1183479c7b
Ivan <ivan.koltsov.1994@mail.com>
2023-07-06 20:45:19 +0500

    add model Lesson

--- course/models.py
+++ course/models.py
@@ -17,3 +17,19 @@ class Course(models.Model):
         verbose_name = 'Курс'
         verbose_name_plural = 'Курсы'
         ordering = ('name')
+
+class Lesson(models.Model):
+    """Класс описания модели урока"""
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='название курса')
+    name = models.CharField(max_length=150,verbose_name="название урока", **NULLABLE)
+    preview =models.ImageField(upload_to="course/",verbose_name="изображение", **NULLABLE)
+    description = models.TextField(verbose_name='описание', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        """Класс мета настроек"""
+        verbose_name = 'Урок'
+        verbose_name_plural = 'Уроки'
+        ordering = ('name')
