b9e14ba6fa0c654013a060d192512d148e1550c7
Ivan <ivan.koltsov.1994@mail.com>
2023-07-06 20:48:35 +0500

    add model User

--- users/models.py
+++ users/models.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.models import AbstractUser, UserManager
 from django.db import models
 
-# Create your models here.
+from course.models import NULLABLE
+
+
+class User(AbstractUser):
+    username = None
+
+    email = models.EmailField(verbose_name='почта', unique=True)
+    phone = models.CharField(max_length=35, verbose_name='номер телефона')
+    city = models.CharField(max_length=100, verbose_name='город')
+    avatar = models.ImageField(upload_to='users/', verbose_name='аватар', **NULLABLE)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+        unique_together = ('email', 'phone')
+        ordering = ('email',)  # сортировка, '-email' - сортировка в обратном порядке
+
+    def __str__(self):
+        return f'{self.email} - {self.phone}: {self.city}'
