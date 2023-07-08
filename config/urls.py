8e2d739acd83328394eba2ced9085c013d58ed90
Ivan <ivan.koltsov.1994@mail.com>
2023-07-08 20:13:37 +0500

    add viewset to users

--- config/urls.py
+++ config/urls.py
@@ -22,5 +22,6 @@ from config import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('courses/', include('course.urls'))
+    path('courses/', include('course.urls')),
+    path('users/', include('users.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
